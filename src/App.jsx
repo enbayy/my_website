e5ca2129beb0fce2ed07a335a1f635b8939b40b2
Enes Bayar <103318928+enbayy@users.noreply.github.com>
2024-09-05 19:36:15 +0300

    edited

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage/HomePage'
 import ProjectsPage from './pages/ProjectsPage/ProjectsPage'
 import Footer from './components/Footer'
+import ContactPage from './pages/ContactPage/ContactPage'
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path='/' element={<HomePage />} />
           <Route path='/projects' element={<ProjectsPage />} />
+          <Route path='/contact' element={<ContactPage />} />
         </Routes>
       </div>
       <Footer />
