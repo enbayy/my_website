7b9caead5424339b2a90ab832e3f600e520b98c1
Enes Bayar <103318928+enbayy@users.noreply.github.com>
2024-09-09 20:59:11 +0300

    cards edited

--- src/Data.jsx
+++ src/Data.jsx
@@ -1,33 +1,51 @@
-import asd from './assets/enbay.jpg'
+import altinToplama from './assets/altinToplama.png'
+import labirent from './assets/labirent.png'
+import puzzle from './assets/puzzle.png'
+import runner from './assets/runner.png'
+import tappyFish from './assets/tappyFish.png'
+import uzay from './assets/uzay.png'
 
-export const projects =
-    [
-        {
-            id: 1,
-            title: "Tappy Fish",
-            description: "kuş zıplamalı oyun",
-            link: "https://www.youtube.com/watch?v=eH_gFI35yFc",
-            image: asd
-        },
-        {
-            id: 2,
-            title: "Tappy Fish",
-            description: "kuş zıplamalı oyun",
-            link: "https://www.youtube.com/watch?v=eH_gFI35yFc",
-            image: asd
-        },
-        {
-            id: 1,
-            title: "Tappy Fish",
-            description: "kuş zıplamalı oyun",
-            link: "https://www.youtube.com/watch?v=eH_gFI35yFc",
-            image: asd
-        },
-        {
-            id: 2,
-            title: "Tappy Fish",
-            description: "kuş zıplamalı oyun",
-            link: "https://www.youtube.com/watch?v=eH_gFI35yFc",
-            image: asd
-        }
-    ]
+export const projects = [
+    {
+        id: 1,
+        title: "Altın Toplama Oyunu",
+        description: "Altınları engellere çarpmadan topla",
+        link: "https://www.youtube.com/watch?v=YE9FJjNm944",
+        image: altinToplama,
+    },
+    {
+        id: 2,
+        title: "Labirent Oyunu",
+        description: "Topu duvarlara çarpmadan bitişe ulaş",
+        link: "https://www.youtube.com/watch?v=4gK_YA7zuac",
+        image: labirent,
+    },
+    {
+        id: 3,
+        title: "Puzzle Oyunu",
+        description: "Parçaları doğru birleştir, puzzle tamamlansın",
+        link: "https://www.youtube.com/watch?v=DCGzFzBRuqo",
+        image: puzzle,
+    },
+    {
+        id: 4,
+        title: "Runner Oyunu",
+        description: "Altınları toplayarak bitişe ulaşmaya çalış",
+        link: "https://www.youtube.com/watch?v=sIIVut53bG4",
+        image: runner,
+    },
+    {
+        id: 5,
+        title: "Tappy Fish Oyunu",
+        description: "Kuşu zıplatarak engellerin arasından geç",
+        link: "https://www.youtube.com/watch?v=eH_gFI35yFc",
+        image: tappyFish,
+    },
+    {
+        id: 6,
+        title: "Uzay Oyunu",
+        description: "Canavarları ateş ederek yok et",
+        link: "https://www.youtube.com/watch?v=CANa6lSmPDc&t=25s",
+        image: uzay,
+    },
+]
