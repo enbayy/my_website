51eecba868ba4f845f2dd5cf2f15eabe96996656
Enes Bayar <103318928+enbayy@users.noreply.github.com>
2024-09-13 19:18:03 +0300

    edited

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,15 +7,12 @@ function Navbar() {
             <div className="navbar-logo">Enes Bayar</div>
             <ul className="navbar-menu">
                 <div className="navbar-menu">
-                    <a className='navbar-link' href="#">Home</a>
-                    <a className='navbar-link' href="#projects">Projects</a>
-                    <a className='navbar-link' href="#contact">Contact</a>
-                    <a className='navbar-link' href="#about">About</a>
+                    <a className='navbar-link' href="#">Giriş</a>
+                    <a className='navbar-link' href="#projects">Çalışmalar</a>
+                    <a className='navbar-link' href="#contact">İletişim</a>
+                    <a className='navbar-link' href="#about">Hakkımda</a>
                 </div>
             </ul>
-            <div className="navbar-search">
-                <input type="text" placeholder="Search..." />
-            </div>
         </nav>
     )
 }
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
-import '../../css/HomePage.css'; // İlgili CSS dosyasını import edin
+import '../../css/HomePage.css';
 
 function HomePage() {
+  const text = "Web Developer";
+
   return (
     <section id="home">
-      <div className="stars"></div> {/* Yıldızlı arka plan */}
-      <div className="home-title">Enes Bayar</div>
+      <div className="animated-background">
+        <div className="home-title-container">
+          <div className="home-name">Enes Bayar</div>
+          <div className="home-subtitle">
+            {text.split("").map((letter, index) => (
+              <span key={index} className="fade-in-out">
+                {letter === " " ? "\u00A0" : letter}
+              </span>
+            ))}
+          </div>
+        </div>
+        <div className="circle one"></div>
+        <div className="circle two"></div>
+        <div className="circle three"></div>
+        <div className="square one"></div>
+        <div className="square two"></div>
+        <div className="circle one"></div>
+        <div className="circle two"></div>
+        <div className="circle three"></div>
+        <div className="square one"></div>
+      </div>
     </section>
   );
 }
