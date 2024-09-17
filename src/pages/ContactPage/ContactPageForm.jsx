a4db83b5f40e59f77adb504e5ad6f5d7aa0dd54c
Enes Bayar <103318928+enbayy@users.noreply.github.com>
2024-09-17 08:14:36 +0300

    Contact page edited

--- src/pages/ContactPage/ContactPageForm.jsx
+++ src/pages/ContactPage/ContactPageForm.jsx
@@ -1,27 +1,60 @@
 import React from 'react'
 import '../../css/ContactPageForm.css'
+import { FaLocationArrow } from "react-icons/fa6";
+import { IoMdMail } from "react-icons/io";
+import { FaPhone } from "react-icons/fa6";
 
 function ContactPageForm() {
     return (
-
         <form className='form-main'>
             <div className='text'>İletişim</div>
+            <div className='info-main'>
+                <div className='info'>
+                    <div className='icon'>
+                        <FaLocationArrow />
+                    </div>
+                    <div className='name'>
+                        Adres
+                    </div>
+                    <div className='desc'>
+                        İzmir
+                    </div>
+                </div>
+                <div className='info'>
+                    <div className='icon'>
+                        <IoMdMail />
+                    </div>
+                    <div className='name'>
+                        E-mail
+                    </div>
+                    <div className='desc'>
+                        1enesbayar@gmail.com
+                    </div>
+                </div>
+                <div className='info'>
+                    <div className='icon'>
+                        <FaPhone />
+                    </div>
+                    <div className='name'>
+                        Telefon
+                    </div>
+                    <div className='desc'>
+                        0 552 231 2086
+                    </div>
+                </div>
+            </div>
             <div className='form'>
-                İsim:
-                <input type="text" placeholder='İsim' />
-                Soyisim:
-                <input type="text" placeholder='Soyisim' />
-                E-mail:
-                <input type="text" placeholder='E-mail' />
-                Metin:
-                <textarea placeholder="Metin Giriniz"></textarea>
+                <div className='row'>
+                    <input type="text" placeholder='İsim' />
+                    <input type="text" placeholder='E-mail' />
+                </div>
+                <textarea placeholder='Mesaj'></textarea>
             </div>
             <div className='buttons'>
                 <button className='button-clear'>Temizle</button>
                 <button className='button-send'>Gönder</button>
             </div>
         </form>
-
     )
 }
 
