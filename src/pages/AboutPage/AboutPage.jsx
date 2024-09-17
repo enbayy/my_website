3c26eefecd68ecc6509c0122d6dc47cabe3e5d73
Enes Bayar <103318928+enbayy@users.noreply.github.com>
2024-09-17 06:14:32 +0300

    About page edited

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 import '../../css/AboutPage.css';
 
 function AboutPage() {
+  const handleScrollToContact = () => {
+    setTimeout(() => {
+      const contactSection = document.getElementById('contact');
+      if (contactSection) {
+        contactSection.scrollIntoView({ behavior: 'smooth' });
+      }
+    }, 100);
+  };
   return (
     <div className="about-container">
       <h1 className="about-title">Kim bu Enes?</h1>
@@ -9,10 +17,10 @@ function AboutPage() {
         <p>Merhaba, ben Enes Bayar. 2024 yılında Selçuk Üniversitesi Bilgisayar Mühendisliği bölümünden mezun oldum. Üniversite hayatım boyunca teknoloji ve yazılım dünyasında çeşitli alanlarda kendimi geliştirme fırsatı buldum.</p>
         <p>Kariyerime mobil uygulama geliştirme alanında başladım ve bu süreçte çeşitli projelerde aktif olarak yer aldım. Mobil platformlarda kullanıcı deneyimini iyileştirmek ve işlevselliği artırmak amacıyla çeşitli uygulama geliştirme çalışmalarında bulundum.</p>
         <p>Sonrasında oyun sektöründe çalışmalara başladım ve bu alanda kapsamlı deneyimler edindim. İki farklı şirkette staj yaparak oyun geliştirme süreçlerinin tüm aşamalarında ve reklam entegrasyonlarında görev aldım. Ancak zamanla web teknolojilerine olan ilgim arttı ve kariyerimi Front-End geliştirme üzerine şekillendirmeye karar verdim. Şu anda aktif olarak Front-End geliştirme alanında çalışmalarımı sürdürüyorum.</p>
-        <p>Global bir şirkette başarılı bir staj dönemini tamamladım. Bu süreçte, tam anlamıyla bir full-stack e-ticaret platformu geliştirdim. Projede, AWS hizmetlerini aktif şekilde kullandım. Kullanıcıların ürünleri görüntüleyebildiği, sepet oluşturabildiği ve güvenli ödeme işlemleri gerçekleştirebildiği bir platform inşa ettim.</p>
-        <p>Bu proje sırasında AWS'nin çeşitli hizmetlerinden faydalandım. AWS Cognito ile kullanıcı kimlik doğrulama ve yetkilendirme işlemlerini gerçekleştirdim. Kullanıcıların güvenli bir şekilde giriş yapabilmesi ve kayıt olabilmesi için güçlü bir kimlik yönetim sistemi oluşturduk. AWS IAM (Identity and Access Management) kullanarak sistemdeki kaynaklara erişimi güvenli hale getirdim ve kullanıcı rollerini tanımladım.</p>
-        <p>Ayrıca, projede AWS S3'ü kullanarak statik dosya yönetimini gerçekleştirdim ve AWS Lambda ile sunucusuz fonksiyonları etkin bir şekilde kullanarak dinamik işlemleri otomatik hale getirdim. Bu sistemler, platformun ölçeklenebilir ve güvenli bir altyapıya sahip olmasını sağladı.</p>
-        <p>Bu deneyim sayesinde AWS'nin bulut bilişim hizmetlerinde derin bir bilgi birikimi kazandım ve tam anlamıyla bir full-stack geliştirici olarak çalıştım. Şu anda kendi projelerim üzerinde çalışmaya devam ediyorum ve gelecekte daha büyük projelerde AWS hizmetlerini etkin bir şekilde kullanmayı hedefliyorum.</p>
+        <p>Global bir şirkette başarılı bir staj dönemini tamamladım. Bu süreçte, AWS hizmetlerini kullanarak full-stack bir e-ticaret platformu geliştirdim. Bu platformda, kullanıcılar ürünleri görüntüleyip sepete ekleyebiliyor ve güvenli ödeme yapabiliyor. AWS Cognito ile kullanıcı kimlik doğrulama ve yetkilendirme işlemlerini gerçekleştirdim ve IAM ile güvenli erişim ve rol yönetimi sağladım. Bu süreç, AWS bulut hizmetlerinde derinlemesine bilgi kazanmamı ve full-stack geliştirme becerilerimi pekiştirmemi sağladı.</p>
+      </div>
+      <div className='contact-button'>
+        <button className='button' onClick={handleScrollToContact}>İletişime Geç</button>
       </div>
     </div>
   );
