d5f1da904cb7699bb9218a6a5d8d2d116aabb9ba
Enes Bayar <103318928+enbayy@users.noreply.github.com>
2024-09-23 15:37:12 +0300

    navbar edited

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,16 +2,7 @@ import React, { useState } from 'react';
 import '../css/Navbar.css';
 
 function Navbar() {
-    const [showDropdown, setShowDropdown] = useState(false);
     const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-    const handleMouseEnter = () => {
-        setShowDropdown(true);
-    };
-
-    const handleMouseLeave = () => {
-        setShowDropdown(false);
-    };
 
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
@@ -26,19 +17,7 @@ function Navbar() {
             <ul className={`navbar-menu ${isMenuOpen ? 'open' : ''}`}>
                 <a className="navbar-link" href="#home">Giriş</a>
                 <a className="navbar-link" href="#about">Hakkımda</a>
-                <li
-                    className="navbar-link"
-                    onMouseEnter={handleMouseEnter}
-                    onMouseLeave={handleMouseLeave}
-                >
-                    Çalışmalar
-                    {showDropdown && (
-                        <ul className="dropdown-menu">
-                            <li><a href="#projects" className="dropdown-link">Web Projeleri</a></li>
-                            <li><a href="#projects" className="dropdown-link">Oyun Projeleri</a></li>
-                        </ul>
-                    )}
-                </li>
+                <a className="navbar-link" href="#projects">Çalışmalar</a>
                 <a className="navbar-link" href="#services">Hizmetler</a>
                 <a className="navbar-link" href="#contact">İletişim</a>
                 <a className="navbar-link" href="#about">CV</a>
